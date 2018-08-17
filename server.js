b3349367ff0cc4837627c73b9cef53bbb87ce1d7
thekaranjit <thekaranjit@gmail.com>
2018-08-18 03:13:17 +0400

    Server with Express with examples

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+
+	res.send("getting root")
+});
+
+app.get('/profile', (req, res) => {
+
+	res.send("getting profile")
+});
+
+app.post('/profile', (req, res) => {
+	console.log(req.body)
+	const user = {
+		name: 'Sally',
+		hobby: 'Soccer'
+	}
+	res.send(user)
+});
+
+
+app.listen(3000);
